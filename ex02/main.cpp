0e3fd3d23b4b0bf78f6b00be5dcdc9e407536841
mmad <mohamed.mad1-etu@etu.univh2c.ma>
2024-12-21 18:59:59 +0100

    Updated

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,54 +1,53 @@
+// #include <Array.hpp>
 #include "Array.hpp"
 
-int main(){
-    // unsigned int size = 10;
-
-    const Array<int> arr(20);
-    // arr[0] = 10;
-    for (int i = 0; i < arr.getSize(); i++){
-        std::cout << arr[i] << std::endl;
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
     }
-    // for (int i = 0; i < arr->getSize(); i++){
-    //     std::cout << arr->at(i) << std::endl;
-    // }
-    // delete arr;
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
     return 0;
 }
-// #include <iostream>
-
-// template <typename T>
-// class Array {
-// private:
-//     T* arr;
-//     int size;
-    
-// public:
-//     Array(int n) : size(n) {
-//         arr = new T[n];
-//     }
-
-//     ~Array() {
-//         delete[] arr;
-//     }
-
-//     T& operator[](int index) {
-//         return arr[index];
-//     }
-
-//     const T& operator[](int index) const {
-//         return arr[index];
-//     }
-
-//     int getSize() const { return size; }
-// };
-
-// int main() {
-//     Array<int> arr(5);  // Create an array of size 5
-//     arr[0] = 100;
-//     arr[1] = 20;
-
-//     std::cout << arr[0] << std::endl;  // Outputs 10
-//     std::cout << arr[1] << std::endl;  // Outputs 20
-
-//     return 0;
-// }
